278445d709c921663e3b566b0652c6e3f0fdf1a6
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-04-08 14:58:29 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//An high level overview of javascript
+
+//javascript is a high level, prototype-based,object-oriented,garbage collected, dynamically typed, interpreted , single threaded,multi-pradigm, non-blocking, concurrency-model programing language
+
+//scripting language
