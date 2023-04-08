6e856fba8d3a5e0b9009a9fc38c05088dd821709
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-04-08 15:15:17 +0600

    add

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 //javascript is a high level, prototype-based,object-oriented,garbage collected, dynamically typed, interpreted , single threaded,multi-pradigm, non-blocking, concurrency-model programing language
 
 //scripting language
+//object--->clone---->serve---->prototype
+//garbagw collected--> As a high level language js automatically keeps its element in memory including some extra files.functionallty in js allows for the automatic removal or deletion of poinless files.
+
+//dynamically typed-
+let num = 10; //number
+num = "10"; //string
+
+//compiled
